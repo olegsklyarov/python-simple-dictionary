264bff7173cb8378b141b674ac7956a61a03fcb3
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2021-06-15 18:29:46 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def handler():
+    word = request.args.get('word')
+    dictionary = {
+        'cat': 'кошка',
+        'dog': 'собака',
+        'table': 'стол',
+    }
+    translation = dictionary.get(word, 'не найдено в словаре')
+
+    return render_template(
+        'form.html',
+        word=word,
+        translation=translation
+    )
